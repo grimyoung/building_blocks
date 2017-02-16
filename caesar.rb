f79a93c97853d4e4680f7c05f1ad0781aea4186d
grimyoung <samhyukim@gmail.com>
2017-02-15 21:01:41 -0800

    caesar cipher

---
+++ caesar.rb
@@ -0,0 +1,21 @@
+#works so long as ascii encoding doesn't change
+
+def caesar(string, shift)
+	cypher =""
+	string.scan(/./) do |letter|
+		num = letter.ord
+		if num >= 65 && num <=90
+			num = (num-65 + (shift.to_i))%26 + 65
+		elsif num >= 97 && num <=122
+			num = (num-97 + (shift.to_i))%26 + 97
+		end
+		cypher += num.chr
+	end
+	cypher
+end
+
+puts "Enter String:"
+input_string = gets.chomp
+puts "Enter shift factor:"
+input_shift = gets.chomp
+puts caesar(input_string,input_shift)
